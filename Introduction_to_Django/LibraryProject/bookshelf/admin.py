ed78e3899d73823a31d30f443fbf8f599e2489fe
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-02-16 19:46:52 +0300

    Register Book model in admin interface and ensure search fields are defined

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,3 +6,5 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('publication_year', 'author')
     search_fields = ('title', 'author')
+    
+admin.site.register(Book, BookAdmin)
