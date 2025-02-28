a1d8517b648b6b340941b4c2ba2aa3d33be4e61c
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-02-28 18:35:27 +0300

    Add UserProfile and Book models with role-based permissions

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -27,3 +27,37 @@ class Librarian(models.Model):
     
     def __str__(self):
         return self.name
+    
+from django.db import models
+from django.contrib.auth.models import User
+
+# Creating user role choices
+ROLE_CHOICES = [
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+]
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+    
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+    
+class Meta:
+    permission = [
+        ('can_add_book', 'Can add book'),
+        ('can_change_book', 'Can edit a book'),
+        ('can_delete_book', 'Can delete book'),
+    ]
+    
+def __str__(self):
+    return self.title
