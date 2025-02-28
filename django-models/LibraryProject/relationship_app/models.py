a70cff3a3ffffebfcb255630fdf3f49be7c84b24
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-02-28 18:42:09 +0300

    Refactor models in relationship app for improved clarity and organization; add UserProfile model with role choices and update permissions for Book model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,35 +3,36 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=255)
-    
+
     def __str__(self):
         return self.name
-    
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
     
 class Library(models.Model):
     name = models.CharField(max_length=255)
     books = models.ManyToManyField(Book)
-    
+
     def __str__(self):
         return self.name
     
 class Librarian(models.Model):
     name = models.CharField(max_length=255)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.name
     
+# extending user model with userprofile module 3
 from django.db import models
 from django.contrib.auth.models import User
 
-# Creating user role choices
+# we add user role choices
 ROLE_CHOICES = [
     ('Admin', 'Admin'),
     ('Librarian', 'Librarian'),
@@ -41,23 +42,24 @@ ROLE_CHOICES = [
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
-    
+
     def __str__(self):
         return f"{self.user.username} - {self.role}"
     
+# defining url patterns for secured views for asgn 4
 from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     published_date = models.DateField()
-    
+
 class Meta:
-    permission = [
-        ('can_add_book', 'Can add book'),
-        ('can_change_book', 'Can edit a book'),
-        ('can_delete_book', 'Can delete book'),
+    permissions = [
+        ("can_add_book", "Can add a book"),
+        ("can_change_book", "Can edit a book"),
+        ("can_delete_book", "Can delete a book"),
     ]
-    
+
 def __str__(self):
     return self.title
