6b884ca872c8566cf8434c7f5bbd93a93174a72d
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-03-02 21:52:16 +0300

    Implement book management views with permission checks and add search functionality

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Book
+
+class ExampleForm(forms.Form):
+    query = forms.CharField(max_length=100, required=True) # Validates user input
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_date']
+        widgets = {
+            'publication_date': forms.DateInput(attrs={'type': 'date'}),
+        }
