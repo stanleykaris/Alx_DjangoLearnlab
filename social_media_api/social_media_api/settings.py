cf2ebfe9f6838042ba827f40045eaf5dad5b4103
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-04-02 11:15:47 +0300

    Change database configuration to use MySQL with specified credentials

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_media_db',
+        'USER': 'root',
+        'PASSWORD': 'kirigo@1999',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
